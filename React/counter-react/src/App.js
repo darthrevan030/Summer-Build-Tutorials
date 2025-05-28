7c6dd94a77645b87566e40870d4d34df2165be45
darth.revan.030 <smartsamarth03@gmail.com>
2025-05-28 15:34:17 +0800

    Refactor code formatting for consistency in App.js and App.css

--- React/counter-react/src/App.js
+++ React/counter-react/src/App.js
@@ -3,36 +3,37 @@ import { useState } from "react";
 
 function App() {
   // array destructuring
-  const [counter, setCounter] = useState(0);
+    const [counter, setCounter] = useState(0);
 
-  function increase() {
+    function increase() {
     setCounter(counter + 1);
-  }
+    }
 
-  function decrease() {
+    function decrease() {
     setCounter(counter - 1);
-  }
+    }
 
-  function reset() {
+    function reset() {
     setCounter(0);
-  }
+    }
 
-  return (
+    return (
     <div className="App">
-      <h1 className={`counter-${counter > 0 ? "positive" : "negative"}`}
+        <button onClick={increase}>Increment</button>
+        <button onClick={decrease}>Decrement</button>
+        <br />
+        <button onClick={reset}>Reset</button>
+        <h1 className={`counter-${counter > 0 ? "positive" : "negative"}`}
         style={{fontSize: 12 + counter*counter}}>{counter}
-      </h1>
-      <button onClick={increase}>Increment</button>
-      <button onClick={decrease}>Decrement</button>
-      <br />
-      <button onClick={reset}>Reset</button>
-      <div className="message">
-        {counter === 0 && <p>Let's go!!!!!</p>}
-        {counter > 0 && counter <= 5 && <p>Just getting started...</p>}
-        {counter < 0 && <p>Wrong way!!!</p>}
-      </div>
+        </h1>
+
+        <div className="message">
+			{counter === 0 && <p>Let's go!!!!!</p>}
+			{counter > 0 && counter <= 5 && <p>Just getting started...</p>}
+			{counter < 0 && <p>Wrong way!!!</p>}
+		</div>
     </div>
-  );
+	);
 }
 
 export default App;
