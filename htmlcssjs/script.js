891c10818e758aa8cdcdfbcf0d8c0961bc527134
darth.revan.030 <smartsamarth03@gmail.com>
2025-05-28 12:16:48 +0800

    First Script for a function

--- htmlcssjs/script.js
+++ htmlcssjs/script.js
@@ -1,1 +1,18 @@
 alert("I AM CONNECTED!!!!!")
+
+function addNumbers(){ // define functions
+    let a = prompt("What is your first number?");
+    let b = prompt("What is your second number?");
+    const result = parseInt(a) + parseInt(b);
+    alert("Your number is " + result);
+}
+
+// Another Way to define functions
+    // const addNumbers = function(){
+    //     code
+    // }
+
+// Another Way to define functions
+    // const addNumbers = () => {
+    //     code
+    // }
