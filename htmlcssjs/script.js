3d03f453563318e90cf20bb60d8dbc207be10236
darth.revan.030 <smartsamarth03@gmail.com>
2025-05-28 12:51:56 +0800

    Add functionality to change background color with a button

--- htmlcssjs/script.js
+++ htmlcssjs/script.js
@@ -38,3 +38,9 @@ const addNumbers = () => {
     resultDiv.innerHTML = "Your result is " + result;
 
 }   
+
+const changeBackgroundColor = () => {
+    // Generate a random hex color
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = randomColor;
+}
